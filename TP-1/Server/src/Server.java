ce78c11405e5de8b9ecf0d3ce85118ffdc621748
Jacob Dorais <jacobdorais25@hotmail.com>
2019-09-17 11:04:57 -0400

    add server-side input verification

---
+++ TP-1/Server/src/Server.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+
+public class Server {
+	
+	private static String ip = "";
+	private static int port = 0;
+	
+	public static void main(String[] args)
+	{
+		while(!init());//call init until we have valid entries
+		
+		System.out.println("hello");
+		
+	}
+	
+	private static Boolean init()
+	{
+		System.out.println("enter ip and port between 5000 and 5050");
+		System.out.println("ex. 192.168.1.101:5000");
+		Scanner userInput = new Scanner(System.in);
+		String firstInput = userInput.nextLine();
+		userInput.close();
+		String[] input = firstInput.split("[:\\.]", 5 + 1);
+		
+		////////////////////////////////////////////////////////////
+		// check for valid format
+		////////////////////////////////////////////////////////////
+		
+		if(input.length != 5)
+		{
+			System.out.println(firstInput + " does not have the good format--> X.X.X.X:X");
+			return false;
+		}
+		
+		////////////////////////////////////////////////////////////
+		// check for valid ip address
+		////////////////////////////////////////////////////////////
+
+		for(int i = 0; i < 4; i++)
+		{
+			int num;
+			try 
+			{
+				num = Integer.parseInt(input[i]);
+			}
+			catch(Exception e)
+			{
+				System.out.println(input[i] + " must be a valid number.");
+				return false;
+			}
+			
+			if(num < 0 || num > 255)
+			{
+				System.out.println("the number: " + input[i] + 
+						" must be between 0 and 255");
+				return false;
+			}
+		}
+
+		ip = 	input[0] + '.' +
+				input[1] + '.' +
+				input[2] + '.' +
+				input[3];
+		
+		////////////////////////////////////////////////////////////
+		// check for valid port number
+		////////////////////////////////////////////////////////////
+		
+		try 
+		{
+			port = Integer.parseInt(input[4]);
+		}
+		catch(Exception e)
+		{
+			System.out.println(input[4] + " must be a valid number.");
+			return false;
+		}
+
+		if(port < 5000 || port > 5050)
+		{
+			System.out.println("port: " + port + " must be between 5000 and 5050");
+			return false;
+		}
+		
+		////////////////////////////////////////////////////////////
+		// all verifications pass
+		////////////////////////////////////////////////////////////
+		return true;
+	}
+	
+}
